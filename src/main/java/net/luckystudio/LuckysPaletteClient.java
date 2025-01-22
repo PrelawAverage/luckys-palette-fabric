6541bd3dfde694b403669f6c34a29352f2955a11
PrelawAverage <bryanguzman859@gmail.com>
2025-01-22 14:58:20 -0500

    - Added the client environment to the client class

--- src/main/java/net/luckystudio/LuckysPaletteClient.java
+++ src/main/java/net/luckystudio/LuckysPaletteClient.java
@@ -1,8 +1,11 @@
 package net.luckystudio;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.luckystudio.client.ModRenderLayers;
 
+@Environment(EnvType.CLIENT)
 public class LuckysPaletteClient implements ClientModInitializer {
 
 	@Override
